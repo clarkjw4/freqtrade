7d829ea87ba82e3e2a928cafe2c3fc6838b432e4
Peter Clark <pjclark@email.sc.edu>
2017-10-25 10:26:52 -0400

    self -> btc_logger

--- rpc/telegram.py
+++ rpc/telegram.py
@@ -142,7 +142,7 @@ def _log(bot: Bot, update: Update) -> None:
     send_msg('Log (Trades) Created: file:///{path}'.format(path=filepath_trades))
 
     # Order Logs
-    limit = self.limit_order_log()
+    limit = btc_logger.limit_order_log()
     order_messages = messager.get_order_log(limit)
     filepath_orders = os.path.dirname(os.path.abspath('order_log.csv'))
     for message in order_messages:
